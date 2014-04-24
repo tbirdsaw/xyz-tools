ca56af865558d4523e65b08eb7a360618bfa3fa9
tbirdsaw <tbirdsaw@cox.net>
2014-04-24 02:05:06 -0700

    ChunkSum should have the functions to generate serial chunks + checksum for XYZDavinci.

--- chunkSum.py
+++ chunkSum.py
@@ -5,9 +5,11 @@ import os
 
 offsetFile = 0;
 
-gcodeFile = "testfiles/test.gcode"
 
-gcode = open(gcodeFile, 'r')
+#Open our file
+gcodeFile = "testfiles/test.gcode"
+gcode = os.fdopen(os.open(gcodeFile,os.O_RDONLY))
+
 
 def errprint(message):
 	sys.stderr.write(message + "\n");
@@ -17,13 +19,10 @@ def checkSum(st):
         # Generate checksum for the given data by adding up all the values
 	return sum(ord(c) for c in st)
 
-def readChunk():
-	global offsetFile;
-        #Read 10236 bytes of data
-	gcode.seek(offsetFile)
+def readChunk(fileOffset):
+        #Seek to the given location and read 10236 bytes of data
+	gcode.seek(fileOffset)
 	dataChunk = gcode.read(10236);
-	#ckChunk = checkSum(dataChunk);
-        #print dataChunk + hex(ckChunk);
 	return dataChunk;
 
 def generateSerialChunk(byteChunk):
@@ -42,18 +41,31 @@ def generateSerialChunk(byteChunk):
 	return dataStream + ckStream
 	
 def readFile():
-	global offsetFile
-	chunkFile = readChunk();
-	offsetFile = offsetFile + 10236
-	print str(len(chunkFile))
-	errprint("OffsetFile is: " + str(offsetFile))
-	while len(chunkFile) == 10236 :
-		errprint( "at position " + str(gcode.tell()) + " in file")
-		#sys.stdout.write(generateSerialData(chunkFile))
-		offsetFile = offsetFile + 10236
-		chunkFile = readChunk();
-		print str(len(chunkFile))
+	# Set our fileOffset so we know where we are in the file
+	fileOffset = 0;
+	# Get file length for percentage calculator
+	fileLen = os.stat(gcodeFile).st_size;
+
+	# Loop until we run out of file
+	while True:
+		# Read our first chunk of file
+		chunkOfFile = readChunk(fileOffset)
+		serialDataChunk = generateSerialChunk(chunkFile)
+		
+		# In the main program, serial handling code and upload code goes here
+		sys.stdout.write(serialDataChunk)
+		
+		# Check to see if the chunk is smaller than 10236, if so, break.
+		if len(chunkFile) != 10236:
+			break
+
+		# Update our offset point
+		fileOffset = fileOffset + 10236
+		
+		# Let the user know how far we are through the file
+		errprint("Processed {:0.2f}% of file.".format((fileOffset / float(fileLen) * 100)))
+
+
 	errprint("Finished")
 
-print os.stat(gcodeFile).st_size
 readFile()
